30e10d520965b2debc2e1e15aa265259766e4ea5
Clif Houck <me@clifhouck.com>
2015-01-28 14:10:09 -0600

    Pulled in IronicClientWrapper from Openstack Nova.
    Also integrated the IronicClientWrapper tests into Arsenal tests.
    Made a few changes to the base test case class to support
    IronicClientWrapper tests.

--- arsenal/tests/base.py
+++ arsenal/tests/base.py
@@ -15,9 +15,18 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from oslo.config import cfg
 from oslotest import base
+
+CONF = cfg.CONF
 
 
 class TestCase(base.BaseTestCase):
 
     """Test case base class for all unit tests."""
+
+    def flags(self, **kw):
+        """Override flag variables for a test."""
+        group = kw.pop('group', None)
+        for k, v in kw.iteritems():
+            CONF.set_override(k, v, group)
