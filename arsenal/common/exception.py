1c2b356d2d33f280f99c6b24fb5b5218a7289590
Clif Houck <me@clifhouck.com>
2015-01-28 12:21:03 -0600

    Added Exception file and base exception class
    Adds exception.py which contains a general base class, ArsenanalException,
    for Arsenal related exceptions. ArsenanalException is adapted from Nova's
    NovaException.

---
+++ arsenal/common/exception.py
@@ -0,0 +1,77 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2015 Rackspace
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+""" Arsenal Exceptions """
+
+import sys
+
+from oslo.config import cfg
+
+from arsenal.openstack.common import log as logging
+
+LOG = logging.getLogger(__name__)
+
+CONF = cfg.CONF
+
+
+# NOTE(ClifHouck): Basically a renamed NovaException from Openstack Nova.
+class ArsenalException(Exception):
+    """Base Arsenal Exception
+
+    To correctly use this class, inherit from it and define
+    a 'msg_fmt' property. That msg_fmt will get printf'd
+    with the keyword arguments provided to the constructor.
+
+    """
+    msg_fmt = "An unknown exception occurred."
+    code = 500
+    headers = {}
+    safe = False
+
+    def __init__(self, message=None, **kwargs):
+        self.kwargs = kwargs
+
+        if 'code' not in self.kwargs:
+            try:
+                self.kwargs['code'] = self.code
+            except AttributeError:
+                pass
+
+        if not message:
+            try:
+                message = self.msg_fmt % kwargs
+
+            except Exception:
+                exc_info = sys.exc_info()
+                # kwargs doesn't match a variable in the message
+                # log the issue and the kwargs
+                LOG.exception('Exception in string format operation')
+                for name, value in kwargs.iteritems():
+                    LOG.error("%s: %s" % (name, value))    # noqa
+
+                if CONF.fatal_exception_format_errors:
+                    raise exc_info[0], exc_info[1], exc_info[2]
+                else:
+                    # at least get the core message out if something happened
+                    message = self.msg_fmt
+
+        super(ArsenalException, self).__init__(message)
+
+    def format_message(self):
+        # NOTE(mrodden): use the first argument to the python Exception object
+        # which should be our full ArsenalException message, (see __init__)
+        return self.args[0]
